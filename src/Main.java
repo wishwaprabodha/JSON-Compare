e4fc7360e4294751b47dfb606bc1548bdb7ab533
wishwaprabodha <ww46673@gmail.com>
2019-02-25 01:56:57 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -48,23 +48,27 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         JSONScanner jsonScanner1 = new JSONScanner(System.in);
-        JSONScanner jsonScanner2 = new JSONScanner(System.in);
+        //JSONScanner jsonScanner2 = new JSONScanner(System.in);
 
         Object object1 = getObject(jsonScanner1);
-        Object object2 = getObject(jsonScanner2);
+        Object object2 = getObject(jsonScanner1);
 
         boolean status = object1.equals(object2);
+        String m1=object1.toString().replace('=',':');
+        String m2=object2.toString().replace('=',':');
 
         if (status) {
             System.out.println("Objects are equal");
+            System.out.println(m2);
         } else {
             System.out.println("Objects are not equal");
+            System.out.println(m1);
+            System.out.println(m2);
         }
 
-        String m1=object1.toString().replace('=',':');
-        String m2=object2.toString().replace('=',':');
+
         System.out.println("**********************");
-        System.out.println(m1);
-        System.out.println(m2);
+
+
     }
 }
