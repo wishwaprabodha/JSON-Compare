77dafd151c74c7e1e397fd07fa8b54ebd4506af8
wishwaprabodha <ww46673@gmail.com>
2019-02-25 00:32:27 +0530

    all added

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Main {
+
+    private static Object getObject(JSONScanner jsonScanner) {
+        String part = jsonScanner.next();
+
+        switch (part) {
+            case "{": {
+                // object
+                Map<String, Object> child = new TreeMap<>();
+
+                String condition = part;
+                Object value;
+
+                while (!condition.equals("}")) {
+                    String key = jsonScanner.next();
+                    jsonScanner.next(); // remove ':'
+                    value = getObject(jsonScanner);
+                    child.put(key, value);
+
+                    condition = jsonScanner.next(); // ',' | '}'
+                }
+
+                return child;
+            }
+            case "[": {
+                // list
+                List<Object> elements = new ArrayList<>();
+
+                Object condition = part;
+
+                while (!condition.equals("]")) {
+                    Object value = getObject(jsonScanner);
+                    elements.add(value);
+
+                    condition = jsonScanner.next(); // ',' | ']'
+                }
+
+                return elements;
+            }
+            default:
+                return part; // string | number | boolean | null
+
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        JSONScanner jsonScanner1 = new JSONScanner(System.in);
+        JSONScanner jsonScanner2 = new JSONScanner(System.in);
+
+        Object object1 = getObject(jsonScanner1);
+        Object object2 = getObject(jsonScanner2);
+
+        boolean status = object1.equals(object2);
+
+        if (status) {
+            System.out.println("Objects are equal");
+        } else {
+            System.out.println("Objects are not equal");
+        }
+
+        String m1=object1.toString().replace('=',':');
+        String m2=object2.toString().replace('=',':');
+        System.out.println("**********************");
+        System.out.println(m1);
+        System.out.println(m2);
+    }
+}
